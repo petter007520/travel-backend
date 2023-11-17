d5e1045e125b2a39e9902353bfa5d2e1646f907d
petter <petter@gmail.com>
2023-11-17 23:14:34 +0800

    update:更新

--- public/mobile/film/js/stripscript.js
+++
@@ -1,8 +0,0 @@
- function stripscript(s) {
-        var pattern = new RegExp("[`~!#$^&*()=|{}':;',\\[\\]<>/?~！#￥¥……&*（）&;—\"|{}【】'；：'。，、？]")
-            var rs = "";
-        for (var i = 0; i < s.length; i++) {
-            rs = rs + s.substr(i, 1).replace(pattern, '');
-        }
-        return rs;
-    }
--- vendor/chenhua/laravel5-markdown-editor/src/routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-//处理markdown编辑器文件上传地址
-Route::post('markdown/upload', function(){
-    $info = MarkdownEditor::upload();
-    return json_encode($info);
-});
