6504dd4639e4f4d5df5eec34cfa97798da5374dc
petter <petter@gmail.com>
2023-12-26 19:16:30 +0800

    update:更新

--- resources/views/hui/act_rewards/lists.blade.php
+++ resources/views/hui/act_rewards/lists.blade.php
@@ -34,7 +34,6 @@
                     <col>
                     <col>
                     <col>
-                    <col>
                 </colgroup>
                 <thead>
                 <tr>
